1c0018e3612605c6c6d6f1a338d86c6015dd2adf
Bundchen <1476193741@qq.com>
2020-04-04 02:06:10 +0800

    删除了先前的homework1 homework2 homework3 和 exercise文件夹

--- exercise/exe 1.06/0226_3.py
+++
@@ -1,8 +0,0 @@
-"""
- 用列表定义10个同学的成绩,输出最高分,最低分,总分和平均值
-"""
-score = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print("最低分:", min(score))
-print(max(score))
-print(sum(score))
-print(sum(score) / len(score))
--- exercise/exe 1.10/0304_1.py
+++
@@ -1,7 +0,0 @@
-'''
-将控制台输入的字符串,转换成元组, 并输出显示
-'''
-demo = input()
-
-tu = tuple(demo.split())
-print(tu)
